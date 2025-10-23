eb924ff9d416de9d73b63224224e60352070d9c7
nicodelos24 <nicodelos24@gmail.com>
2025-10-23 04:44:19 -0300

    agrego formulario de ingredientes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 .Programa que permite guardar productos y calcular su precio de costo
 
+
 Para luego poder ajustar y visualizar su precio, ganancia, y porcentaje de ganancia.
 También ingresando cuánto porcentaje de ganancia se quiere ganar se puede ver el precio correspondiente.
 
@@ -7,17 +8,30 @@ También ingresando cuánto porcentaje de ganancia se quiere ganar se puede ver 
 
 
 - Y ahora quiero integrar un inventario para poder aprovechar que se puede guardar una lista de ingredientes,
-colocando así su precio al por mayor del producto, y que se realicen los cálculos para poder calcular el costo
+colocando así el precio al por mayor del producto, y que se realicen los cálculos para poder obtener el costo
 de las recetas (o sea el producto final) y así tener siempre precios actualizados y correctos.
 
-- Quizá agregar también funcionalidad de tener las imágenes del inventario guardadas si se puede (no se de que forma
+-opc- Quizá agregar también funcionalidad de tener las imágenes del inventario guardadas si se puede (no se de que forma
 porque creo que localStorage era limitado).
 
-- Luego puede ser también tener el recetario guardado o algo así si se puede, donde ya se calculen precios por los 
-ingredientes de cada receta y sus cantidades correspondienties.
 
-- Otra idea puede ser la de agregar un estimado de horas (tiempo) que toma cada receta.
+-opc- Otra idea puede ser la de agregar un estimado de horas (tiempo) que toma cada receta.
+
+-opc- Luego puede ser también tener el recetario guardado o algo así si se puede, donde ya se calculen precios por los 
+ingredientes de cada receta y sus cantidades correspondienties.
 
 Aprovechando esa funcionalidad se podría calcular cuánto del porcentaje de ganancia corresponde al tiempo que lleva 
 o algo así, por ejemplo saber cuánto estaría costando la hora de trabajo y así tener un conteo de la ganancia por el
 tiempo invertido, algo así como una especie de forma de saber cuánto se gana por hora, o por proyecto, quizá teniendo un registro de eso si se puede.
+
+
+En conclusion:
+
+- Debe poder calcular Ingresos, Costos variables (ingredientes), Costos fijos (gas, decoración, transporte, )
+
+
+-poder ingresar el precio o generarlo a traves de los gastos
+como ingredientes (que se sumen según cada receta, acá estaria bueno meter el recetario) 
+
+
+
--- js/script.js
+++ js/script.js
@@ -2,23 +2,46 @@ const form = document.getElementById("form-prod");
 const tabla = document.getElementById("tabla-prod");
 const total = document.getElementById("ganancia-total");
 
+const formIng = document.getElementById("form-ing");
+const tablaIng = document.getElementById("tabla-ing").querySelector("tbody");
+
 const costoInput = document.getElementById("costo");
 const precioInput = document.getElementById("precio");
 const imagenInput = document.getElementById("imagen");
 const ingredientesInput = document.getElementById("ingredientes");
 const gananciaInput = document.getElementById("ganancia-deseada");
 
-const productos = JSON.parse(localStorage.getItem('productos')) || [];
+let ingredientes = JSON.parse(localStorage.getItem("ingredientes")) || [];
+const productos = JSON.parse(localStorage.getItem("productos")) || [];
 
 let gananciaTotal = 0;
 
-
+// esto es lo de leer la imagen y convertir a Base64 y lo guarda en localStorage
 function leerImagen(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = e => resolve(e.target.result);
-    reader.onerror = e => reject(e);
+    reader.onload = (e) => resolve(e.target.result);
+    reader.onerror = (e) => reject(e);
     reader.readAsDataURL(file);
+  });
+}
+// voy por acá haciendo eso, así es el orden?
+function agregarIngredienteATabla(ing) {
+  const fila = document.createElement("tr");
+  fila.innerHTML = `
+  <td>${ing.nombre}</td>
+  <td>${ing.cantidad}</td>
+  <td>${ing.unidad}</td>
+  <td>$${ing.precio.toFixed(2)}</td>
+  <td>$${(ing.precio / ing.cantidad).toFixed(2)} por ${ing.unidad}</td>
+  <td><button class= "borrar-ing">🗑️</button></td>
+  `;
+  tablaIng.appendChild(fila);
+
+  fila.querySelector(".borrar-ing").addEventListener("click", () => {
+    ingredientes = ingredientes.filter((i) => i.nombre !== ing.nombre);
+    localStorage.setItem("ingredientes", JSON.stringify(ingredientes));
+    fila.remove();
   });
 }
 
@@ -62,23 +85,28 @@ form.addEventListener("submit", async (e) => {
   //const ganancia = precio - costo;
   const porcentaje = ((precioFinal - costo) / costo) * 100;
 
-
-  
   let imagen = "";
   if (imagenInput.files[0]) {
     imagen = await leerImagen(imagenInput.files[0]);
   }
 
-
-  const ingredientes = ingredientesInput.value
+  const ingredientesProd = ingredientesInput.value
     .split(",")
-    .map(i => i.trim())
-    .filter(i => i);
+    .map((i) => i.trim())
+    .filter((i) => i);
 
+  const id = productos.length ? productos[productos.length - 1].id + 1 : 1;
 
-  const id = productos.length ? productos[productos.length -1].id + 1 : 1;
-
-  const producto = { id, nombre, costo, precio: precioFinal, ganancia, porcentaje, imagen, ingredientes };
+  const producto = {
+    id,
+    nombre,
+    costo,
+    precio: precioFinal,
+    ganancia,
+    porcentaje,
+    imagen,
+    ingredientes : ingredientesProd,
+  };
   productos.push(producto);
   localStorage.setItem("productos", JSON.stringify(productos));
 
@@ -87,13 +115,34 @@ form.addEventListener("submit", async (e) => {
   gananciaTotal += ganancia;
   total.textContent = gananciaTotal.toFixed(2);
 
-
   form.reset();
   precioInput.disabled = false;
   gananciaInput.disabled = false;
 });
 
-function agregarProductoATabla(prod){
+formIng.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nombre = document.getElementById("ing-nombre").value.trim();
+  const cantidad = parseFloat(document.getElementById("ing-cantidad").value);
+  const unidad = document.getElementById("ing-unidad").value.trim();
+  const precio = parseFloat(document.getElementById("ing-precio").value);
+
+  if (!nombre || isNaN(cantidad) || cantidad<= 0 || isNaN(precio) || precio <= 0) {
+    alert("Por favor completa todos los campos del ingrediente. ");
+    return;
+  }
+
+const nuevoIng = { nombre, cantidad, unidad, precio};
+ingredientes.push(nuevoIng);
+localStorage.setItem("ingredientes",JSON.stringify(ingredientes));
+agregarIngredienteATabla(nuevoIng);
+
+
+formIng.reset();
+});
+
+function agregarProductoATabla(prod) {
   const fila = document.createElement("tr");
   fila.innerHTML = `
   <td>${prod.nombre}</td>
@@ -105,11 +154,6 @@ function agregarProductoATabla(prod){
   <td>${prod.imagen ? `<img src="${prod.imagen}" width="50">` : ""}</td>
   `;
   tabla.querySelector("tbody").appendChild(fila);
-
-  }
-  productos.forEach(agregarProductoATabla);
-
-
-
-
-
+}
+productos.forEach(agregarProductoATabla);
+ingredientes.forEach(agregarIngredienteATabla);
